f23663439805270921caf2e54b8282dc1e7f81a9
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2026-04-18 00:44:02 +0200

    fix api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { seed } from './seeding/seeding.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
