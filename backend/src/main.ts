c496996b4eca0cfecbb36d01950b0693f249ea1d
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2026-04-18 00:18:11 +0200

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,12 @@ import { seed } from './seeding/seeding.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await seed();
   await app.listen(process.env.PORT ?? 3000);
 }
