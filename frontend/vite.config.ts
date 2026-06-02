9838533ece577a817294a7b51dcbe24d01f5a2a6
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2026-06-02 21:46:32 +0200

    update allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,18 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+//import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  //plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
-    allowedHosts: ['0.0.0.0'],
+    allowedHosts: ['0.0.0.0', 'boekenkast.vervoortkobe.be.eu.org'],
   },
   define: {
     __VUE_PROD_DEVTOOLS__: false,
